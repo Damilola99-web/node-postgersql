d2e2e6429c5c2db033c83a50cc5724772a323ac4
Damilola99-web <rasheedw622@gmail.com>
2022-10-31 09:11:15 +0100

    new

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
-const {Pool} = require('pg')
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env
+	.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env
+	.DB_DATABASE}`;
 
 const pool = new Pool({
-    user: 'postgres',
-    database: 'todo_database',
-    host: 'localhost',
-    password: 'korede',
-    port: 5432
-})
+	connectionString :
+		isProduction ? process.env.DATABASE_URL :
+		connectionString
+});
 
-module.exports = pool
+module.exports = pool;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const pool = require('./db')
 const Router = require('./routes/todoRoutes')
 
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.use(express.json())
@@ -12,6 +13,6 @@ app.use((req, res) => {
     res.status(404).json({message : "404 page not found"})
 })
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('listening')
 })
